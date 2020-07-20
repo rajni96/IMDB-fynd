00b97796d0532c722c76df412cb7526f2826af39
Rajni <rajnibkwr2015@gmail.com>
2020-07-20 08:52:56 +0530

    whitenoise added

--- IMDB/wsgi.py
+++ IMDB/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IMDB.settings')
 
 application = get_wsgi_application()
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
